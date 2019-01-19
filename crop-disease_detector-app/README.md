3295cb5f9cec94cbf82ba75a57d0e6a26229643a
Shubhajitdas121 <shubhajitdas121@gmail.com>
2019-01-19 20:34:31 +0530

    crop-disease_detector-app

---
+++ crop-disease_detector-app/README.md
@@ -0,0 +1,19 @@
+# fastai-vision-app
+
+
+Production ready starter pack for creating fast responsive Web App for Fast.AI Image models using Starlette.io framework with Uvicorn ASGI server.
+
+Everything packaged in docker with requirement.txt, so you can push it to any docker hosted cloud service. Enjoy :)
+
+Few dockers hosted services where this starter pack will work =>
+
+
+* https://render.com
+* https://zeit.co/now
+* https://azure.microsoft.com/en-us/services/app-service/containers/
+* https://getcarina.com/
+* https://sloppy.io/en/
+* https://giantswarm.io
+* https://aws.amazon.com/ecs/
+* https://cloud.google.com/cloud-build/docs/
+* https://www.digitalocean.com/products/one-click-apps/docker/
---
+++ crop-disease_detector-app/app/server.py
@@ -0,0 +1,65 @@
+from starlette.applications import Starlette
+from starlette.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn, aiohttp, asyncio
+from io import BytesIO
+from fastai.vision import *
+import base64
+
+model_file_url = 'https://www.dropbox.com/s/thdw37v58zwlaa5/stage-2_34-256p.pth?raw=1'
+model_file_name = 'model'
+classes = ['Apple___Apple_scab','Apple___Black_rot','Apple___Cedar_apple_rust','Apple___healthy','Blueberry___healthy','Cherry_(including_sour)___Powdery_mildew','Cherry_(including_sour)___healthy','Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot','Corn_(maize)___Common_rust_','Corn_(maize)___Northern_Leaf_Blight','Corn_(maize)___healthy','Grape___Black_rot','Grape___Esca_(Black_Measles)','Grape___Leaf_blight_(Isariopsis_Leaf_Spot)','Grape___healthy','Orange___Haunglongbing_(Citrus_greening)','Peach___Bacterial_spot','Peach___healthy',
+'Pepper,_bell___Bacterial_spot','Pepper,_bell___healthy','Potato___Early_blight','Potato___Late_blight','Potato___healthy','Raspberry___healthy','Soybean___healthy','Squash___Powdery_mildew','Strawberry___Leaf_scorch','Strawberry___healthy','Tomato___Bacterial_spot','Tomato___Early_blight','Tomato___Late_blight','Tomato___Leaf_Mold','Tomato___Septoria_leaf_spot','Tomato___Spider_mites Two-spotted_spider_mite','Tomato___Target_Spot','Tomato___Tomato_Yellow_Leaf_Curl_Virus','Tomato___Tomato_mosaic_virus','Tomato___healthy','background']
+
+path = Path(__file__).parent
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='app/static'))
+
+async def download_file(url, dest):
+    if dest.exists(): return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(dest, 'wb') as f: f.write(data)
+
+async def setup_learner():
+    await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
+    data_bunch = ImageDataBunch.single_from_classes(path, classes, tfms=get_transforms(), size=256).normalize(imagenet_stats)
+    learn = create_cnn(data_bunch, models.resnet34, pretrained=False)
+    learn.load(model_file_name)
+    return learn
+
+loop = asyncio.get_event_loop()
+tasks = [asyncio.ensure_future(setup_learner())]
+learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+loop.close()
+
+PREDICTION_FILE_SRC = path/'static'/'predictions.txt'
+
+@app.route("/upload", methods=["POST"])
+async def upload(request):
+    data = await request.form()
+    img_bytes = await (data["img"].read())
+    bytes = base64.b64decode(img_bytes)
+    return predict_from_bytes(bytes)
+
+def predict_from_bytes(bytes):
+    img = open_image(BytesIO(bytes))
+    _,_,losses = learn.predict(img)
+    predictions = sorted(zip(classes, map(float, losses)), key=lambda p: p[1], reverse=True)
+    result_html1 = path/'static'/'result1.html'
+    result_html2 = path/'static'/'result2.html'
+    
+    result_html = str(result_html1.open().read() +str(predictions[0:3]) + result_html2.open().read())
+    return HTMLResponse(result_html)
+
+@app.route("/")
+def form(request):
+    index_html = path/'static'/'index.html'
+    return HTMLResponse(index_html.open().read())
+
+if __name__ == "__main__":
+    if "serve" in sys.argv: uvicorn.run(app, host="0.0.0.0", port=8080)
